6e54483b200d0d5c4d69570ab1626cc211f567f1
mahfuzar175 <mahfuzar175@gmail.com>
2023-11-26 11:25:36 +0600

    create a api for delete cart from database

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ktpzdpn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,42 +26,57 @@ async function run() {
     // await client.connect();
 
     const userCollection = client.db("travelDB").collection("users");
-    const packagesCollection = client.db("travelDB").collection("travelPackages");
+    const packagesCollection = client
+      .db("travelDB")
+      .collection("travelPackages");
     const storyCollection = client.db("travelDB").collection("stories");
     const cartCollection = client.db("travelDB").collection("carts");
 
     // users related apis
-    app.post('/users', async(req, res) =>{
+    app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
       res.send(result);
-  })
+    });
 
     // packages apis
-    app.get('/travelPackages', async(req, res) =>{
-        const result = await packagesCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/travelPackages", async (req, res) => {
+      const result = await packagesCollection.find().toArray();
+      res.send(result);
+    });
 
     // stories apis
-    app.get('/stories', async(req, res) =>{
-        const result = await storyCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/stories", async (req, res) => {
+      const result = await storyCollection.find().toArray();
+      res.send(result);
+    });
 
-    
-// cart related apps
-    app.post('/carts', async(req, res) =>{
+    // cart related apps
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
-  })
+    });
 
-
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -71,11 +84,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send("travel is running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`travel is running on port ${port}`);
-  });
+  res.send("travel is running");
+});
+
+app.listen(port, () => {
+  console.log(`travel is running on port ${port}`);
+});
