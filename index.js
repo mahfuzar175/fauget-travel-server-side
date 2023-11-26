d1ea410ad4c32b5c8642d55067fa6cb4ad569a80
mahfuzar175 <mahfuzar175@gmail.com>
2023-11-26 12:34:18 +0600

    user data are send to the database

--- index.js
+++ index.js
@@ -33,11 +33,24 @@ async function run() {
     const cartCollection = client.db("travelDB").collection("carts");
 
     // users related apis
+    // app.get("/users", async (req, res) => {
+    //   const email = req.query.email;
+    //   const query = { email: email };
+    //   const result = await userCollection.find(query).toArray();
+    //   res.send(result);
+    // });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already exist', insertedId: null})
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
 
     // packages apis
     app.get("/travelPackages", async (req, res) => {
